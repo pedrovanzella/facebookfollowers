7d1c06f48cb51937b358a4bca019d0b704b2e1bd
Pedro Vanzella <pedro@pedrovanzella.com>
2011-07-16 13:13:06 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebookfollowers::Application.config.secret_token = '12781cf0339cf483d6d0413ad7b34d56d30e5865e5d8d569c640e56fb4a310d4b34aaabdb24b6a111f7d1301d31d0b215c158bb05b968a240d9507dd7d41c502'
