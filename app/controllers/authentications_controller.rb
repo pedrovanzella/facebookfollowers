629825e5ab0a222c6b92a8c50331321f69a4fa89
Pedro Vanzella <pedro@pedrovanzella.com>
2011-07-16 20:50:42 -0300

    prettying shit up

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,8 +4,8 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-    auth = request.env["omniauth.auth"]
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+    omniauth = request.env["omniauth.auth"]
+    current_user.authentications.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     flash[:notice] = "Authentication successful."
     redirect_to authentications_url
   end
