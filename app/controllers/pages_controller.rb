b213a4f50e85885481e7e92aadd41f3213fb265d
Pedro Vanzella <pedro@pedrovanzella.com>
2011-07-17 20:24:50 -0300

    now we have a list of only facebook friends and one of only twitter firends

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,19 @@
 class PagesController < ApplicationController
   def index
+    @facebook_friends = current_user.facebook.fetch.friends
+    @twitter_friends = current_user.twitter.friends.users
+
+   # @facebook_friends.reject!{ |ff| ff.name == @twitter_friends.each { |tf| tf.name } }
+   # @twitter_friends.reject!{ |tf| tf.name == @facebook_friends.each { |ff| ff.name } }
+
+   current_user.twitter.friends.users.each do |tf|
+     @facebook_friends.reject!{ |ff| ff.name == tf.name }
+   end
+
+   current_user.facebook.fetch.friends.each do |ff|
+     @twitter_friends.reject!{ |tf| tf.name == ff.name }
+   end
+
   end
 
 end
