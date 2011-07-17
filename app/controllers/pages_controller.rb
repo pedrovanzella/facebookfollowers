95a0edfe9e0f26d114c7ddd351410b49bb9c3510
Pedro Vanzella <pedro@pedrovanzella.com>
2011-07-17 20:59:01 -0300

    tabelas nojentas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,21 @@
 class PagesController < ApplicationController
   def index
-    @facebook_friends = current_user.facebook.fetch.friends
-    @twitter_friends = current_user.twitter.friends.users
+    if current_user
+      if current_user.facebook
+        if current_user.twitter
+          @facebook_friends = current_user.facebook.fetch.friends
+          @twitter_friends = current_user.twitter.friends.users
 
-   # @facebook_friends.reject!{ |ff| ff.name == @twitter_friends.each { |tf| tf.name } }
-   # @twitter_friends.reject!{ |tf| tf.name == @facebook_friends.each { |ff| ff.name } }
+           current_user.twitter.friends.users.each do |tf|
+             @facebook_friends.reject!{ |ff| ff.name == tf.name }
+           end
 
-   current_user.twitter.friends.users.each do |tf|
-     @facebook_friends.reject!{ |ff| ff.name == tf.name }
-   end
-
-   current_user.facebook.fetch.friends.each do |ff|
-     @twitter_friends.reject!{ |tf| tf.name == ff.name }
-   end
+           current_user.facebook.fetch.friends.each do |ff|
+             @twitter_friends.reject!{ |tf| tf.name == ff.name }
+           end
+        end
+      end
+    end
 
   end
 
